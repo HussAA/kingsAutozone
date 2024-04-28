942c47fb765e94cc971e185b1282147fcc359029
Hussaa <mah.hussaina@gmail.com>
2024-04-28 01:51:48 -0400

    reusable components

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-
+import 'aos/dist/aos.css';
 import "./src/styles/index.css"
 import "./src/styles/packages.css"
 import "./src/styles/services.css"
 import "./src/styles/Gallery.css"
+import './src/styles/ReusableComponents.css'
 import React from "react"
 import Providers from "./src/providers"
 
